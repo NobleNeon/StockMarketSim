4ea30a2dafa61ad52fb30c8ffe7c9a77b5ce4c8d
NobleNeon <97766824+NobleNeon@users.noreply.github.com>
2024-06-03 17:26:57 +0000

    added basic methods for buy, sell, short, cover

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,32 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
+
+    public double buyStock(String ticker, int shares){
+        double cost = 0;
+        double lastPrice = 0;
+        cost = lastPrice * shares;
+        return cost;
+    }
+    
+    public double sellStock(String ticker, int shares){
+        double value = 0;
+        double lastPrice = 0;
+        value = lastPrice * shares;
+        return value;
+    }
+
+    public double shortStock(String ticker, int shares){
+        double value = 0;
+        double lastPrice = 0;
+    value = lastPrice * shares;
+        return value;
+    }
+
+    public double cover(String ticker, int shares){
+        double value = 0;
+        double lastPrice = 0;
+        value = lastPrice * shares;
+        return value;
+    }
 }
