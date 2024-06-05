6e93f937bf558c1bc829e1e22fb8783381d1e9b9
sharayhbadar <sharayhbadar@gmail.com>
2024-06-05 13:22:44 -0400

    Updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+package main.java;
+
+import java.io.IOException;
+
 public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Stock tesla = new Stock("TSLA");
 
--- src/main/java/Stock.java
+++ src/main/java/Stock.java
@@ -1,3 +1,9 @@
+package main.java;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import com.jayway.jsonpath.JsonPath;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -5,10 +11,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
-import com.jayway.jsonpath.JsonPath;
 
 public class Stock {
 
