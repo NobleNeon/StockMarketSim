5f768528f1490c000f368a8c72f5d736924261b6
amirn <nafissi.amir@gmail.com>
2024-06-10 10:28:26 -0400

    Updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main{
 
 //        System.out.println(tesla.getCompanyName());
 
-        System.out.println("Bismillah");
+        System.out.println("bismillah");
         new LogInPage();
 
     }
