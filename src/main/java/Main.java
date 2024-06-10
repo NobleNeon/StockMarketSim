ec312ab235d6c983f2c332723f58907552b5fbaa
Amir Nafissi <nafissi.amir@gmail.com>
2024-06-10 11:48:42 -0400

    Updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main{
 
 //        System.out.println(tesla.getCompanyName());
 
-        System.out.println("SKIBIDI TOILET");
+        new MainAppFrame();
         new LogInPage();
 
     }
