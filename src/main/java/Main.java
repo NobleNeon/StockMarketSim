19efe31954517d59918bf6d4bc0f90fb6ac075cb
spark <brodinyoung06@gmail.com>
2024-06-11 13:08:41 -0400

    Updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,38 @@
 package main.java;
 
-import java.io.IOException;
-import java.util.ArrayList;
+
+import java.io.*;
+import java.util.*;
 
 public class Main{
-
-    //public 'ArrayList' matrix to store user's info
-    public static ArrayList<ArrayList<String>> userDataMatrix = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
 //        Stock tesla = new Stock("TSLA");
 
 //        System.out.println(tesla.getCompanyName());
-
-        System.out.println("BIDIBIDIBI TOILET");
+        new MainAppFrame();
+        System.out.println("SKIBIDI TOILET");
         new LogInPage();
+        txtReader();
 
     }
 
+    public static void txtReader() throws IOException {
+        File myFile = new File("user_name.txt");
+        Scanner readFile = new Scanner(myFile);
+        ArrayList<ArrayList<String>> userData = new ArrayList<ArrayList<String>>();
 
+        while (readFile.hasNext()){
+            String[] tokens = readFile.nextLine().split(", ");
+            for (int i = 0; i < tokens.length; i++){
+                userData.add(new ArrayList());
+            }
+            for (int i = 0; i < tokens.length; i++){
+                userData.get(i).add(tokens[i]);
+            }
+            System.out.println(userData);
+        }
+
+    }
 }
